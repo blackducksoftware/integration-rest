f0ccc8d70bd4a87578867346dc30f2e13e04d3e9
Gavin Killough <gavink@synopsys.com>
2019-05-28 10:10:17 -0400

    Add JsonPatch functionality as separate method call in IntJsonTransformer

--- src/main/java/com/synopsys/integration/rest/service/IntJsonTransformer.java
+++ src/main/java/com/synopsys/integration/rest/service/IntJsonTransformer.java
@@ -22,9 +22,20 @@
  */
 package com.synopsys.integration.rest.service;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.flipkart.zjsonpatch.JsonDiff;
+import com.flipkart.zjsonpatch.JsonPatch;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -43,6 +54,10 @@ public class IntJsonTransformer {
         this.gson = gson;
         this.logger = logger;
     }
+
+    // =============================
+    // Basic Response Transformation
+    // =============================
 
     public <R extends IntRestResponse> R getResponse(Response response, Type responseType) throws IntegrationException {
         final String json = response.getContentString();
@@ -66,9 +81,9 @@ public class IntJsonTransformer {
         try {
             addJsonAsField(jsonObject);
             C transformedResponse = gson.fromJson(jsonObject, responseType);
-            transformedResponse.setJsonElement(jsonObject);
 
-            // TODO find a way to transform JsonPatch field from responses
+            // Set the JsonElement of only the root object
+            transformedResponse.setJsonElement(jsonObject);
             return transformedResponse;
         } catch (final JsonSyntaxException e) {
             logger.error(String.format("Could not parse the provided jsonElement with Gson:%s%s", System.lineSeparator(), json));
@@ -76,6 +91,7 @@ public class IntJsonTransformer {
         }
     }
 
+    // Recursively add json as field of every inner object
     private void addJsonAsField(JsonElement jsonElement) {
         if (jsonElement.isJsonObject()) {
             final JsonObject innerObject = jsonElement.getAsJsonObject();
@@ -89,6 +105,63 @@ public class IntJsonTransformer {
                 addJsonAsField(arrayElement);
             }
         }
+    }
+
+    // ==========
+    // Patch Json
+    // ==========
+
+    public void setPatch(IntRestResponse intRestResponse, ObjectMapper objectMapper) {
+        String lossyJson = gson.toJson(intRestResponse);
+
+        try {
+            JsonNode source = objectMapper.readTree(lossyJson);
+            JsonNode target = objectMapper.readTree(intRestResponse.getJson());
+            JsonNode patch = JsonDiff.asJson(source, target);
+            intRestResponse.setPatch(patch);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String producePatchedJson(IntRestResponse intRestResponse, ObjectMapper objectMapper) {
+        String lossyJson = gson.toJson(intRestResponse);
+        try {
+            JsonNode target = objectMapper.readTree(lossyJson);
+            JsonNode patch = intRestResponse.getPatch();
+
+            List<JsonNode> listOfPatches = transformPatchToListOfPatches(patch);
+            for (JsonNode singleChangePatch : listOfPatches) {
+                try {
+                    target = JsonPatch.apply(singleChangePatch, target);
+                } catch (Exception e) {
+                    logger.warn("Could not apply a particular change - this may not be an issue if change involves an object that wasn't being updated: " + e.getMessage());
+                }
+            }
+
+            StringWriter stringWriter = new StringWriter();
+            objectMapper.writeValue(stringWriter, target);
+
+            return stringWriter.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<JsonNode> transformPatchToListOfPatches(JsonNode patch) {
+        List<JsonNode> listOfPatches = new ArrayList<>();
+
+        Iterator<JsonNode> patchIterator = patch.iterator();
+        while (patchIterator.hasNext()) {
+            JsonNode change = patchIterator.next();
+
+            ArrayNode changeArray = JsonNodeFactory.instance.arrayNode(1);
+            changeArray.add(change);
+
+            listOfPatches.add(changeArray);
+        }
+
+        return listOfPatches;
     }
 
 }
