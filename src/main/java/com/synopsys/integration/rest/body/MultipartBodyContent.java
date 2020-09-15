3b1fbf21d738f1ab196435fe11c62e530dbe57ee
Eric Kerwin <ekerwin@synopsys.com>
2020-09-15 16:18:15 -0400

    fixing character support to include utf8

--- src/main/java/com/synopsys/integration/rest/body/MultipartBodyContent.java
+++ src/main/java/com/synopsys/integration/rest/body/MultipartBodyContent.java
@@ -23,15 +23,13 @@
 package com.synopsys.integration.rest.body;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.entity.ContentType;
-import org.apache.http.entity.mime.FormBodyPart;
-import org.apache.http.entity.mime.FormBodyPartBuilder;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.entity.mime.content.StringBody;
 
 import com.synopsys.integration.rest.request.Request;
 
@@ -39,19 +37,19 @@ public class MultipartBodyContent implements BodyContent {
     private final Map<String, String> bodyContentStringMap;
     private final Map<String, File> bodyContentFileMap;
 
-    public MultipartBodyContent(final Map<String, File> bodyContentFileMap, final Map<String, String> bodyContentStringMap) {
+    public MultipartBodyContent(Map<String, File> bodyContentFileMap, Map<String, String> bodyContentStringMap) {
         this.bodyContentStringMap = bodyContentStringMap;
         this.bodyContentFileMap = bodyContentFileMap;
     }
 
     @Override
-    public HttpEntity createEntity(final Request request) {
-        final MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-        for (final Entry<String, File> entry : getBodyContentFileMap().entrySet()) {
+    public HttpEntity createEntity(Request request) {
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        for (Entry<String, File> entry : getBodyContentFileMap().entrySet()) {
             builder.addBinaryBody(entry.getKey(), entry.getValue());
         }
-        for (final Entry<String, String> entry : getBodyContentStringMap().entrySet()) {
-            addTextBody(builder, entry.getKey(), entry.getValue());
+        for (Entry<String, String> entry : getBodyContentStringMap().entrySet()) {
+            builder.addTextBody(entry.getKey(), entry.getValue(), ContentType.TEXT_PLAIN.withCharset(StandardCharsets.UTF_8));
         }
         return builder.build();
     }
@@ -64,13 +62,4 @@ public class MultipartBodyContent implements BodyContent {
         return bodyContentFileMap;
     }
 
-    private void addTextBody(final MultipartEntityBuilder builder, final String name, final String value) {
-        // should be 'builder.addTextBody(entry.getKey(), entry.getValue());'
-        // BUT Black Duck fails to parse form pieces with the header Content-Type
-        // So we must remove that header. For more info see https://jira.dc1.lan/browse/IDETECT-514
-        final StringBody body = new StringBody(value, ContentType.DEFAULT_TEXT);
-        final FormBodyPart part = FormBodyPartBuilder.create(name, body).build();
-        part.getHeader().removeFields("Content-Type");
-        builder.addPart(part);
-    }
 }
